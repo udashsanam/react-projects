2b3f0db849330ed0d8e2269ce23a879ee2edfe8c
sanam udash <udashsanam@gmail.com>
2022-09-20 21:30:34 +0545

    add dyanamic css on the js

--- 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,36 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
+  let css = 'form-control';
 
   const goalInputChangeHandler = event => {
+
+    console.log(event.target.value);
+    if(event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    console.log(enteredValue);
+    
+    if(enteredValue.trim().length === 0){
+        setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+    {/* inside the ` <value> ` treated as string  */}
+      <div className={!isValid? 'form-control invalid':`form-control`}>
+        <label >Course Goal</label>
+        <input 
+         type="text" onChange={goalInputChangeHandler}  />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
